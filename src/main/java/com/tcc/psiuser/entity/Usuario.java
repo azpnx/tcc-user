7c8c9611b91dd1cef72c78cedf4e265843efaae7
Gabriel Garcez <gabrieldouradogarcez@gmail.com>
2023-04-29 17:03:32 -0300

    Alterando links e removendo professor

--- src/main/java/com/tcc/psiuser/entity/Usuario.java
+++ src/main/java/com/tcc/psiuser/entity/Usuario.java
@@ -18,8 +18,7 @@ public class Usuario {
     @Column(unique = true, length = 60)
     private String email;
     private String password;
-    private String nome;
-    private String professor;
+    private String name;
     @Enumerated(EnumType.STRING)
     private AppUserRoleEnum role;
     private Boolean blocked = false;
